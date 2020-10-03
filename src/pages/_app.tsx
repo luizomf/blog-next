66fe4da14287c3e53c9e90bf64025d8a26654d09
Otávio Miranda <luizomf@gmail.com>
2020-10-03 01:34:28 -0300

    Corrigindo AppProps

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import { AppProps } from 'next/dist/next-server/lib/router/router';
+import { AppProps } from 'next/app';
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyles } from '../styles/global-styles';
 import { theme } from '../styles/theme';
