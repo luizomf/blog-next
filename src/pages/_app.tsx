d0f52905990fdb30d6de6f73c7f2ed0b1e7f2b4f
Otávio Miranda <luizomf@gmail.com>
2020-10-03 01:03:17 -0300

    Configurando o styled-components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,15 @@
-import '../styles/globals.css';
+import { AppProps } from 'next/dist/next-server/lib/router/router';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyles } from '../styles/global-styles';
+import { theme } from '../styles/theme';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+      <GlobalStyles />
+    </ThemeProvider>
+  );
 }
 
 export default MyApp;
