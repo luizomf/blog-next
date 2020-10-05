84837dee0c61ea17ca70b529a03ac2b7a53b8bb3
Otávio Miranda <luizomf@gmail.com>
2020-10-05 16:59:54 -0300

    Component PostCover

--- src/containers/Post/index.tsx
+++ src/containers/Post/index.tsx
@@ -2,6 +2,7 @@ import { Footer } from '../../components/Footer';
 import { Header } from '../../components/Header';
 import { Heading } from '../../components/Heading';
 import { MainContainer } from '../../components/MainContainer';
+import { PostCover } from '../../components/PostCover';
 import { PostData } from '../../domain/posts/post';
 
 export type PostProps = {
@@ -15,6 +16,7 @@ export const Post = ({ post }: PostProps) => {
 
       <MainContainer>
         <Heading>{post.title}</Heading>
+        <PostCover coverUrl={post.cover.formats.large.url} alt={post.title} />
         <div dangerouslySetInnerHTML={{ __html: post.content }} />
       </MainContainer>
 
