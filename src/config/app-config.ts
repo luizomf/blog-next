cf4d1d2ab8366f1d6d7451bfae055f7f047786a8
Otávio Miranda <luizomf@gmail.com>
2020-10-05 18:43:49 -0300

    Adicionando url de produçao

--- src/config/app-config.ts
+++ src/config/app-config.ts
@@ -2,4 +2,4 @@ export const API_URL = 'https://evening-retreat-45125.herokuapp.com';
 export const POSTS_URL = `${API_URL}/posts`;
 
 export const SITE_NAME = 'Otávio Miranda';
-export const SITE_URL = process.env.SITE_URL || 'http://127.0.0.1:3000';
+export const SITE_URL = 'https://dreamy-wilson-f8ff91.netlify.app';
