c190bb6680073df57366b34800e7c527b9efbec5
Otávio Miranda <luizomf@gmail.com>
2020-10-04 22:30:29 -0300

    NextJS SSG com getStaticProps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,33 @@
-export default function Home() {
-  return <h1>Oi</h1>;
+import { GetStaticProps } from 'next';
+import { PostData } from '../domain/posts/post';
+
+const getPosts = async (): Promise<PostData[]> => {
+  const posts = await fetch(
+    'https://evening-retreat-45125.herokuapp.com/posts',
+  );
+  const jsonPosts = await posts.json();
+  return jsonPosts;
+};
+
+export type HomeProps = {
+  posts: PostData[];
+};
+
+export default function Home({ posts }: HomeProps) {
+  return (
+    <div>
+      {posts.map((post) => (
+        <h2 key={post.slug}>{post.title}</h2>
+      ))}
+    </div>
+  );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const posts = await getPosts();
+
+  return {
+    props: { posts },
+    // revalidate: 5,
+  };
+};
