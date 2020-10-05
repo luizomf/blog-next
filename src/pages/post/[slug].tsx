fcf79b86e0b10ef78e153cf9dbd697065ecfbf28
Otávio Miranda <luizomf@gmail.com>
2020-10-05 15:08:15 -0300

    NextJS - getStaticPaths + getStaticProps

---
+++ src/pages/post/[slug].tsx
@@ -0,0 +1,40 @@
+import { GetStaticPaths, GetStaticProps } from 'next';
+import { countAllPosts } from '../../data/posts/count-all-posts';
+import { getAllPosts } from '../../data/posts/get-all-posts';
+import { getPost } from '../../data/posts/get-post';
+import { PostData } from '../../domain/posts/post';
+
+export type DynamicPostProps = {
+  post: PostData;
+};
+
+const DynamicPost = ({ post }: DynamicPostProps) => {
+  return <p>{post.title}</p>;
+};
+
+export default DynamicPost;
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const numberOfPosts = await countAllPosts();
+  const posts = await getAllPosts(`_limit=${numberOfPosts}`);
+
+  return {
+    paths: posts.map((post) => {
+      return {
+        params: {
+          slug: post.slug,
+        },
+      };
+    }),
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  const posts = await getPost(ctx.params.slug);
+
+  return {
+    props: { post: posts[0] },
+    // revalidate: 5,
+  };
+};
