036a363c6c7ad0dd2ebd77ee472d1bf8371aa045
Otávio Miranda <luizomf@gmail.com>
2020-10-05 08:17:15 -0300

    Componente Footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,5 @@
+import { Container } from './styled';
+
+export const Footer = () => {
+  return <Container>Feito com carinho por Otávio Miranda</Container>;
+};
--- src/containers/HomePage/index.tsx
+++ src/containers/HomePage/index.tsx
@@ -3,6 +3,7 @@ import { PostData } from '../../domain/posts/post';
 import { Header } from '../../components/Header';
 import { MainContainer } from '../../components/MainContainer';
 import { PostCard } from '../../components/PostCard';
+import { Footer } from '../../components/Footer';
 
 export type HomePageProps = {
   posts: PostData[];
@@ -24,6 +25,7 @@ export default function HomePage({ posts }: HomePageProps) {
           ))}
         </Container>
       </MainContainer>
+      <Footer />
     </>
   );
 }
