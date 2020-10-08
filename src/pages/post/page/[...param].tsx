5c332c12a9c58595c5bd9765fd771f2015d5e002
Luiz Otavio <luizomf@gmail.com>
2020-10-07 21:43:03 -0300

    Alterado postsPerPage para 9

--- src/pages/post/page/[...param].tsx
+++ src/pages/post/page/[...param].tsx
@@ -31,7 +31,7 @@ export const getStaticPaths: GetStaticPaths = async () => {
 export const getStaticProps: GetStaticProps = async (ctx) => {
   const page = Number(ctx.params.param[0]);
   const category = ctx.params.param[1] || '';
-  const postsPerPage = 6;
+  const postsPerPage = 9;
   const startFrom = (page - 1) * postsPerPage;
 
   const nextPage = page + 1;
