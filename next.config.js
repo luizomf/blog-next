06b9839bc56181907b0757413b52e39deca95226
Luiz Otavio <luizomf@gmail.com>
2020-10-08 02:47:16 -0300

    PWA

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
-module.exports = {
+/* eslint-disable @typescript-eslint/no-var-requires */
+const nextPWA = require('next-pwa');
+const production = process.env.NODE_ENV === 'production';
+
+module.exports = nextPWA({
   // Target must be serverless
   target: 'serverless',
-};
+  pwa: {
+    dest: 'public',
+    disable: !production,
+  },
+});
